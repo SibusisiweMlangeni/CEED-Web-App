5044217480983fa3f016e0c23ee78cfe3171155f
Mbali Puso <mbalipuso@gmail.com>
2025-10-22 18:53:16 +0200

    changed the h1

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -3,7 +3,7 @@ export default function AboutUs({ content }) {
 
   return (
     <section className="p-8 text-center">
-      <h2 className="text-3xl font-bold mb-4">About Us</h2>
+      <h2 className="text-3xl font-bold mb-4">About Us Mbali</h2>
       <p className="max-w-2xl mx-auto">{content || "Content coming soon..."}</p>
     </section>
   );
