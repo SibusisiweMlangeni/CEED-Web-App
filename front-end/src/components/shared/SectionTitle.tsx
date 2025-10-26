c0186009ee80085b11910c93dbe69f2625001699
Mbali Puso <mbalipuso@gmail.com>
2025-10-26 16:12:58 +0200

    section title component

--- front-end/src/components/shared/SectionTitle.tsx
+++ front-end/src/components/shared/SectionTitle.tsx
@@ -8,7 +8,7 @@ type SectionTitleProps = {
 const SectionTitle = ({ title, subtitle }: SectionTitleProps) => {
   return (
     <div className='max-w-[1290px] relative text-center mb-[50px] mx-auto'>
-        <h2 className="mb-5 xl:mb-[30px] text-[40px] xl:text-5xl">{title}</h2>
+        <h2 className="mb-5 xl:mb-[30px] text-[40px] xl:text-5xl font-bold">{title}</h2>
         <p className='text-paragraph w-[70%] mx-auto text-lg xl:text-xl'>{subtitle}</p>
     </div>
   )
