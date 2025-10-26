52779d0a045a71abea39570ddb23753c68df2666
Mbali Puso <mbalipuso@gmail.com>
2025-10-26 17:55:16 +0200

    footer component added

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Lexend } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default async function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
