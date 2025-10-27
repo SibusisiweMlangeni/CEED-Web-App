0483f0b0ebf04e450b884f829ecce9c42adbb39f
Mbali Puso <mbalipuso@gmail.com>
2025-10-27 13:17:09 +0200

    added titleColor and subtitleColor props

--- front-end/src/components/shared/SectionTitle.tsx
+++ front-end/src/components/shared/SectionTitle.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 type SectionTitleProps = {
     title: string;
     subtitle: string;
+    titleColor?: string;
+    subtitleColor?: string;
 }
 
-const SectionTitle = ({ title, subtitle }: SectionTitleProps) => {
+const SectionTitle = ({ title, subtitle, titleColor, subtitleColor }: SectionTitleProps) => {
   return (
-    <div className='max-w-[1290px] relative text-center mb-[50px] mx-auto'>
-        <h2 className="mb-5 xl:mb-[30px] text-[40px] xl:text-5xl font-bold">{title}</h2>
-        <p className='text-paragraph w-[70%] mx-auto text-lg xl:text-xl'>{subtitle}</p>
+    <div className='max-w-[900px] relative text-center mb-[50px] mx-auto'>
+        <h2 className={`mb-5 text-${titleColor} xl:mb-[30px] text-[40px] xl:text-5xl font-bold`}>{title}</h2>
+        <p className={`text-${subtitleColor} w-[70%] mx-auto text-lg xl:text-xl`}>{subtitle}</p>
     </div>
   )
 }
